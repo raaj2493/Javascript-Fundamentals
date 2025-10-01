74300d9f42c8c7866ad681f1b1ec7aad2ced0f74
Raaj Giri <145034794+raaj2493@users.noreply.github.com>
2025-10-01 19:53:48 +0530

    Delete Node JS/01_Events directory

--- Node JS/01_Events/MyEventError.js
+++
@@ -1,9 +0,0 @@
-const { error } = require("console");
-const Eventemitter = require("events");
-const eventemitter = new Eventemitter();
-
-eventemitter.on("error", (error) => (
-    console.log(`error occured ${error.message}`)
-) )
-
-eventemitter.emit("error", new Error("Something went wrong"));
