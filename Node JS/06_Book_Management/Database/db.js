52104ac1b8dc34c33892e31b44f896cb78387933
Raaj Giri <raj.ash2493@gmail.com>
2025-10-02 09:17:14 +0530

    Node JS

--- Node JS/06_Book_Management/Database/db.js
+++ Node JS/06_Book_Management/Database/db.js
@@ -4,3 +4,18 @@ exports.books = [
     { id: 2, title: "Atomic Habits", author: "James Clear" },
     { id: 3, title: "1984", author: "George Orwell" }
 ];
+
+
+const { drizzle } = require("drizzle-orm/node-postgres");
+const { Pool } = require("pg");
+const schema = require("../Drizzle/Schemas");
+
+// Create a connection pool
+const pool = new Pool({
+    connectionString: "postgres://postgres:postgres@localhost:5432/mydb"
+});
+
+// Initialize Drizzle with schema
+const db = drizzle(pool, { schema });
+
+module.exports = db;
